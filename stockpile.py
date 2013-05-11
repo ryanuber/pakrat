27ffb06307e3effad0dcf97a3b37b1ee4bf3de18
Ryan Uber <ru@ryanuber.com>
2013-05-11 10:39:57 -0700

    Downloader now works and stuffs packages into a directory of your choosing.

--- stockpile.py
+++ stockpile.py
@@ -1,35 +1,39 @@
 import yum
 
-repos = [
-    {
-        'name':       'centos-base',
-        'mirrorlist': 'http://mirrorlist.centos.org/?release=6.4&arch=x86_64&repo=os',
-        'arch':       ['x86-64']
-    },
-    {
-        'name':    'centos-updates',
-        'baseurl': 'http://mirror.centos.org/centos/6/updates/x86_64',
-        'arch':    ['x86_64']
-    },
-    {
-        'name':    'epel',
-        'baseurl': 'http://dl.fedoraproject.org/pub/epel/6/x86_64',
-        'arch':    ['x86_64']
-    },
-]
+conf = {
+    'mirror_dir': '/root/mirrors',
+    'repos': [
+        {
+            'name':       'centos-base',
+            'mirrorlist': 'http://mirrorlist.centos.org/?release=6.4&arch=x86_64&repo=os',
+            'arch':       ['x86-64']
+        },
+        {
+            'name':    'centos-updates',
+            'baseurl': 'http://mirror.centos.org/centos/6/updates/x86_64',
+            'arch':    ['x86_64']
+        },
+        {
+            'name':    'epel',
+            'baseurl': 'http://dl.fedoraproject.org/pub/epel/6/x86_64',
+            'arch':    ['x86_64']
+        },
+    ]
+}
 
 yb = yum.YumBase()
 yb.repos.disableRepo('*')
+yb.setCacheDir(force=True, reuse=False, tmpdir=yum.misc.getCacheDir())
 
-for repo in repos:
+for repo in conf['repos']:
     if 'mirrorlist' in repo.keys():
-        yb.add_enable_repo(repo['name'], mirrorlist=repo['mirrorlist'])
+        yr = yb.add_enable_repo(repo['name'], mirrorlist=repo['mirrorlist'])
     elif 'baseurl' in repo.keys():
-        yb.add_enable_repo(repo['name'], baseurls=[repo['baseurl']])
+        yr = yb.add_enable_repo(repo['name'], baseurls=[repo['baseurl']])
     if 'arch' in repo.keys() and type(repo['arch']) is list:
         yb.doSackSetup(thisrepo=repo['name'], archlist=repo['arch'])
 
-result = []
+    yr._dirSetAttr('pkgdir', '%s/%s/packages' % (conf['mirror_dir'], repo['name']))
+
 packages = yb.doPackageLists(pkgnarrow='available', showdups=False)
-for pkg in packages:
-    print '%s-%s.%s' % (pkg.name, pkg.version, pkg.arch)
+yb.downloadPkgs(packages)
