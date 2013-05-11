7db4fe8edfb09cbd1229c28f06948a5f043baedb
Ryan Uber <ru@ryanuber.com>
2013-05-10 22:25:58 -0700

    Updating stockpile

--- stockpile.py
+++ stockpile.py
@@ -4,8 +4,21 @@ import json
 
 @get('/')
 def root():
+    repos = [
+        #{ 'name': 'centos-base', 'mirrorlist': 'http://mirrorlist.centos.org/?release=6&arch=x86_64&repo=os' },
+        #{ 'name': 'centos-updates', 'baseurl': 'http://mirror.centos.org/centos/6/updates/x86_64' }
+        { 'name': 'epel', 'baseurl': 'http://dl.fedoraproject.org/pub/epel/6/x86_64' }
+    ]
+
     yb = yum.YumBase()
-    #add_enable_repo(self, repoid, baseurls=[], mirrorlist=None)
+    yb.repos.disableRepo('*')
+
+    for repo in repos:
+        if 'mirrorlist' in repo.keys():
+            yb.add_enable_repo(repo['name'], mirrorlist=repo['mirrorlist'])
+        elif 'baseurl' in repo.keys():
+            yb.add_enable_repo(repo['name'], baseurls=[repo['baseurl']])
+
     result = []
     packages = yb.doPackageLists(pkgnarrow='available')
     for pkg in packages:
