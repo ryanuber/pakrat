9e40e2e214d105c56f4679fadddc53ae38782f55
Ryan Uber <ru@ryanuber.com>
2013-05-18 14:35:03 -0700

    Fixing mirrorlist issue by passing in a generic prebaseconf object rather than calling doGenericSetup(), which disables all plugins...

--- stockpile.py
+++ stockpile.py
@@ -7,8 +7,7 @@ class StockpileYumBase(yum.YumBase):
 
     def __init__(self):
         yum.YumBase.__init__(self)
-        self.doGenericSetup()
-        self._delRepos()
+        self.preconf = yum._YumPreBaseConf()
         self.setCacheDir(force=True, reuse=False, tmpdir=yum.misc.getCacheDir())
         self.repos.repos = {}
 
