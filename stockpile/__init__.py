935943d70038d20eb4e2d785955ad60471459366
Ryan Uber <ru@ryanuber.com>
2013-06-02 11:49:11 -0700

    Separating CLI functionality so it can be installed separately.

--- stockpile/__init__.py
+++ stockpile/__init__.py
@@ -73,19 +73,3 @@ class exception(Exception):
 
     def __str__(self):
         return repr(self.message)
-
-if __name__ == '__main__':
-
-    from optparse import OptionParser
-
-    parser = OptionParser()
-    parser.add_option('--dest', dest='dest')
-    parser.add_option('-d', '--repodir', action='append', default=[])
-    parser.add_option('-f', '--repofile', action='append', default=[])
-    options, args = parser.parse_args()
-
-    if not options.dest:
-        print '--dest is required'
-        sys.exit(1)
-
-    sync(options.dest, repofiles=options.repofile, repodirs=options.repodir)
---
+++ stockpile/cli.py
@@ -0,0 +1,14 @@
+import stockpile
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('--dest', dest='dest')
+parser.add_option('-d', '--repodir', action='append', default=[])
+parser.add_option('-f', '--repofile', action='append', default=[])
+options, args = parser.parse_args()
+
+if not options.dest:
+    print '--dest is required'
+    sys.exit(1)
+
+stockpile.sync(options.dest, repofiles=options.repofile, repodirs=options.repodir)
