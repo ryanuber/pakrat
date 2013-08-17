36dfd935a4e435a09fcbcb545454ec6994c3fd6a
Ryan Uber <ru@ryanuber.com>
2013-08-16 18:07:45 -0700

    Prep 0.3.0 release

--- pakrat/__init__.py
+++ pakrat/__init__.py
@@ -29,7 +29,7 @@ import signal
 import urlparse
 from pakrat import util, log, repo, repos, progress
 
-__version__ = '0.2.5'
+__version__ = '0.3.0'
 
 def sync(basedir=None, objrepos=[], repodirs=[], repofiles=[],
          repoversion=None, delete=False, callback=None):
--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ required_module('yum')
 required_module('createrepo')
 
 setup(name='pakrat',
-    version='0.2.5',
+    version='0.3.0',
     description='A Python library for mirroring and versioning YUM repositories',
     author='Ryan Uber',
     author_email='ru@ryanuber.com',
